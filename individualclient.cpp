312bb7b9cda6b72c87e1b66b58658b0abf07cd42
EnlightenedCSF <adamant94@mail.ru>
2015-03-12 01:21:35 +0400

    Errors were fixed

--- individualclient.cpp
+++ individualclient.cpp
@@ -3,8 +3,8 @@ using namespace std;
 #include <string>
 
 #include "individualclient.h"
-#include "account.h"
 #include "bank.h"
+#include "account.h"
 
 IndividualClient::IndividualClient(string name) : Client(name)
 {
@@ -22,14 +22,8 @@ bool IndividualClient::TransferFunds(double amount, int fromId, int toId) {
         return false;
 
     accounts_->at(fromId)->WithdrawFunds(amount);
-
-    Account* account = accounts_->at(fromId);
-    Bank* bank = account->GetBank();
-    double forfeit = bank->GetForfeit();
-
-
-  //  accounts_->at(fromId)->GetBank()->Charge(amount / 100.0 * forfeit);
-  //  accounts_->at(toId)->DepositFunds(amount / 100.0 * (100 - forfeit));
-
+    double forfeit = accounts_->at(fromId)->GetBank()->GetForfeit();
+    accounts_->at(fromId)->GetBank()->Charge(amount / 100.0 * forfeit);
+    accounts_->at(toId)->DepositFunds(amount / 100.0 * (100 - forfeit));
     return true;
 }
